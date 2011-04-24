e9cd42084a66d8f285bfe6c79ac22bae3479653d
Trou <devel@syscall.eu>
2011-04-25 01:51:24 +0200

    first checksum working

--- parse.c
+++ parse.c
@@ -5,9 +5,9 @@
 uint16_t checksum(uint8_t *data, int len)
 {
   int i;
-  uint16_t csum=0;
+  int csum=0;
   for (i=0; i<len; i++) {
-      csum = (73 * csum + data[i++]) % 0xFFEF;
+      csum = (73*csum + data[i])%0xFFEF;
   }
   return csum;
 }
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
 	FILE *raw;
 	int offset;
 	uint16_t data[16]; 
+	uint8_t *data8 = (uint8_t *)data;
 	int i;
 
 	if (argc < 3) {
@@ -33,11 +34,11 @@ int main(int argc, char *argv[])
 	printf("Opened %s at offset %x\n",argv[1], offset);
 	fseek(raw, offset, SEEK_SET);
 	fread(data, sizeof(uint16_t), 16, raw);
-	for (i=0; i<16; i++)
-		printf("%04x ", data[i]);
+	for (i=0; i<32; i++)
+		printf("%02x ", data8[i]);
 	printf("\n");
 
-	printf("%x\n", checksum((uint8_t *)(data+1),30));
+	printf("%x\n", checksum(data8+4,12));
 
 	fclose(raw);
 	return 0;
