77f2d2366d7f3d095868c003ecba499cd49ea2af
Trou <devel@syscall.eu>
2011-04-25 02:00:46 +0200

    checksum check ok

--- parse.c
+++ parse.c
@@ -12,12 +12,35 @@ uint16_t checksum(uint8_t *data, int len)
   return csum;
 }
 
+int verify_checksums(uint16_t *data)
+{
+	int i;
+	uint8_t *data8 = (uint8_t *)data;
+	uint8_t even[16], odd[16]; 
+	uint16_t csum1, csum2, csum3, csum4;
+	int res;
+
+	for (i=0; i<16; i++) {
+		even[i] = data8[i*2];
+		odd[i] = data8[i*2+1];
+	}
+	csum1=checksum(data8+4,12);
+	csum2=checksum(data8+16,12);
+	csum3=checksum(even+1,14);
+	csum4=checksum(odd+1,14);
+	res = 0;
+	res ^= (csum1 ^ data[1]);
+	res ^= (csum2 ^ data[14]);
+	res ^= (csum3 ^ data[0]);
+	res ^= (csum4 ^ data[15]);
+	return res;
+}
+
 int main(int argc, char *argv[])
 {
 	FILE *raw;
 	int offset;
 	uint16_t data[16]; 
-	uint8_t even[16], odd[16]; 
 	uint8_t *data8 = (uint8_t *)data;
 	int i;
 
@@ -35,19 +58,15 @@ int main(int argc, char *argv[])
 	printf("Opened %s at offset %x\n",argv[1], offset);
 	fseek(raw, offset, SEEK_SET);
 	fread(data, sizeof(uint16_t), 16, raw);
-	for (i=0; i<16; i++) {
-		printf("%02x ", data8[i*2]);
-		printf("%02x ", data8[i*2+1]);
-		even[i] = data8[i*2];
-		odd[i] = data8[i*2+1];
-
+	for (i=0; i<32; i++) {
+		printf("%02x ", data8[i]);
 	}
 	printf("\n");
+	if (verify_checksums(data))
+		printf("Checksum NOK\n");
+	else
+		printf("Checksum OK\n");
 
-	printf("%x\n", checksum(data8+4,12));
-	printf("%x\n", checksum(data8+16,12));
-	printf("%x\n", checksum(even+1,14));
-	printf("%x\n", checksum(odd+1,14));
 
 	fclose(raw);
 	return 0;
