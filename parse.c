331b079e53d509209bf6a3dd3c9a9c8b0f7bd8d5
Trou <devel@syscall.eu>
2011-04-30 16:18:09 +0200

    add gpl headers, copying, README

--- parse.c
+++ parse.c
@@ -1,3 +1,22 @@
+/* 
+ * Panasonic RW2 files lens distortion correction information parser
+ * Copyright 2011 Raphaël Rigo
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -75,7 +94,6 @@ int main(int argc, char *argv[])
 	FILE *raw;
 	int offset = 0x340;
 	int16_t data[16]; 
-/*	uint8_t *data8 = (uint8_t *)data;*/
 	int i;
 	struct dist d;
 	int opt, mod = 0, index = 0, replace = 0;
